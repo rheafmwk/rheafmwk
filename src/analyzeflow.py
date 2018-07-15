f6ca193ee68b5e84095e3012fee2eb30c24cc262
David Haselberger <david.haselberger@gmail.com>
2018-07-15 14:02:44 +0200

    initial scripts

---
+++ src/analyzeflow.py
@@ -0,0 +1,80 @@
+import spacy
+from spacy.tokens.doc import Doc
+from nltk import tokenize
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import rake
+from py2neo import Graph
+import operator
+import io
+import os
+import sys
+import json
+import re
+
+g = Graph(password="neo5j")
+fileDir = os.path.dirname(os.path.realpath('__file__'))
+stoppath = os.path.join(fileDir, "src/static/assets/SmartStoplist.txt")
+rake_object = rake.Rake(stoppath, 5, 3, 2)
+nlp = spacy.load('en')
+loc = os.path.join(fileDir, "src/static/assets/flowsSerialized")
+docs = []
+namedict = g.run('MATCH (n:Flow) RETURN n.title, n.doc_id, n.description, n.referenceURL').data()
+
+for file in os.listdir(loc):
+    if file.endswith(".bin"):
+        #print(file)
+        filename = re.sub('\.bin$', '', file)
+        myid = ""
+        mydesc = ""
+        myref = ""
+        for item in namedict:
+            if item["n.doc_id"] == filename:
+                myid = item["n.title"]
+                mydesc = item["n.description"]
+                myref = item["n.referenceURL"]
+        for byte_string in Doc.read_bytes(open(os.path.join(loc,file), 'rb')):
+           doc = Doc(nlp.vocab)
+           doc.from_bytes(byte_string)
+           #print(myid)
+           doc.user_data['id']=myid
+           doc.user_data['desc']=mydesc
+           doc.user_data['ref']=myref
+           docs.append(doc)
+
+
+def getanalysis(flow):
+
+    text = flow['description']
+    keywlist = []
+    keywords = rake_object.run(text)
+    for item in keywords:
+        keywlist.append([item[0],item[1]])
+    #print("keywords:")
+    #print(keywords)
+    #print("sentiment:")
+    lines_list = tokenize.sent_tokenize(text)
+    sid = SentimentIntensityAnalyzer()
+    i = 0
+    score = 0.0
+    for sentence in lines_list:
+        sisc = sid.polarity_scores(sentence)
+        score = score + sisc['compound']
+        i = i + 1
+    finalscore = score/i
+    #print(finalscore)
+    ranking = {}
+    mydoc = nlp(text)
+    for doc in docs:
+        myid = doc.user_data['id']
+        mydesc = doc.user_data['desc']
+        myref = doc.user_data['ref']
+        if(myid):
+            score = mydoc.similarity(doc)
+            ranking.update({score: [myid, mydesc, myref]})
+        #print(doc.text)
+    sorted_x = sorted(ranking.items(), key=operator.itemgetter(0), reverse=True)
+    print(sorted_x)
+    rdict = {"sentiment":finalscore, "keywords":keywlist, "flowranking":sorted_x}
+    return (json.dumps(rdict))
+    
+    
---
+++ src/app.py
@@ -0,0 +1,48 @@
+import ontologytojson
+import analyzeflow
+import flask
+from flask import Flask, request, session, redirect, url_for, render_template, flash
+import json
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return flask.render_template("index.html")
+
+@app.route('/flowtemplate', methods=['GET'])
+def flowtemplate():
+    return flask.render_template("flowtemp.html")
+    
+    
+@app.route('/ontologyview', methods=['GET'])
+def ontologyview():
+    return flask.render_template("ontologyview.html")
+    
+@app.route("/ontologydata")
+def ontologydata():
+    return ontologytojson.getontologydata()
+
+@app.route("/node/<nodename>", methods=['GET'])
+def node(nodename):
+    myname = nodename.split(".")[-1]
+    if nodename.startswith("solarbird.flow"):
+        return ontologytojson.getflowdata(myname)
+    elif nodename.startswith("solarbird.coreflow"):
+        return ontologytojson.getcfdata(myname)
+    elif nodename.startswith("solarbird.behaviour"):
+        return ontologytojson.getbehaviourdata(myname)
+    elif nodename.startswith("solarbird.risk"):
+        return ontologytojson.getriskdata(myname)
+    elif nodename.startswith("solarbird.upphase"):
+        return ontologytojson.getupdata(myname)
+    elif nodename.startswith("solarbird.emotion"):
+        return ontologytojson.getemotiondata(myname)
+
+
+@app.route('/analyze_flow', methods=['POST'])
+def analyze_flow():
+    content = request.get_json(silent=True)
+    #print(content)
+    return analyzeflow.getanalysis(content)
+
